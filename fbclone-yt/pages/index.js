621a5bf9aca551115e22b9c5f454345d8db22c0a
Tommy <tommyaudet@spaghetticode.ca>
2021-11-23 11:58:49 -0500

    premier commit, ajout de la navBar

---
+++ fbclone-yt/pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Facebook</title>
+      </Head>
+
+      <Header />
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+  );
+}
